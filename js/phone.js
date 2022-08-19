117ad2da194c6e7374ef5c1e59965cf78cde7f43
Sayem92 <sayemmiha123@gmail.com>
2022-08-19 08:42:50 +0600

    phone rate done

---
+++ js/phone.js
@@ -0,0 +1,37 @@
+function updatePhoneNumber(isIncrease){
+    const phoneNumberfield = document.getElementById('phone-number-field')
+    const phoneNumberfieldString = phoneNumberfield.value;
+    const previousPhoneNumber = parseInt(phoneNumberfieldString);
+
+   let newPhoneNumber;
+
+   if(isIncrease){
+         newPhoneNumber = previousPhoneNumber + 1;
+   }
+   else{
+         newPhoneNumber = previousPhoneNumber - 1;
+   }
+   phoneNumberfield.value = newPhoneNumber;
+
+   return newPhoneNumber;
+}
+
+function updatePhoneTotalPrice(newPhoneNumber){
+    const phoneTotalPrice = newPhoneNumber * 1219;
+   const phoneTotalElement = document.getElementById('phone-total');
+   phoneTotalElement.innerText = phoneTotalPrice;
+
+}
+
+document.getElementById('btn-phone-plus').addEventListener('click', function(){
+   const newPhoneNumber = updatePhoneNumber(true);
+
+    updatePhoneTotalPrice(newPhoneNumber)
+  
+});
+
+document.getElementById('btn-phone-minus').addEventListener('click', function(){
+    const newPhoneNumber = updatePhoneNumber(false);
+
+    updatePhoneTotalPrice(newPhoneNumber)
+});
